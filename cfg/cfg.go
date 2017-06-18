6f0183fb7202ae3ca076ebe8eeccf4dc7327a441
prasadbalpa <prasad.nala@gmail.com>
2017-06-18 12:57:33 +0530

    Create cfg.go

---
+++ cfg/cfg.go
@@ -0,0 +1,30 @@
+package cfg
+
+import (
+	"iAccounts/utils"
+)
+
+const (
+	CASSANDRA_CLUSTER         = "127.0.0.1"
+	ORGANIZATION_KEYSPACE     = "organization"
+	ORGUSERMAP_KEYSPACE       = "orgusermap"
+	ORGID_KEYSPACE            = "org_"
+	HTTP_HEADER_CONTENT_TYPE  = "Content-type"
+	HTTP_HEADER_DATATYPE_JSON = "application/json"
+	LOGIN_MOBILE_NUMBER       = 1
+	LOGIN_OTP_NUMBER          = 2
+	LOGIN_SUCCESSFUL          = 3
+)
+
+func GetCassandraClusters() string {
+	return CASSANDRA_CLUSTER
+}
+func GetOrganizationKeySpace() string {
+	return ORGANIZATION_KEYSPACE
+}
+func GetOrgUserMapKeySpace() string {
+	return ORGUSERMAP_KEYSPACE
+}
+func GetOrgIDKeySpace(orgid string) string {
+	return (ORGID_KEYSPACE + utils.FindAndReplace(orgid, "-", "_"))
+}
