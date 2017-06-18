8435b70557cd42675aad46cf5c0f591ad4ffd737
prasadbalpa <prasad.nala@gmail.com>
2017-06-18 12:56:58 +0530

    Update cassandra.go

--- cassandra/cassandra.go
+++ cassandra/cassandra.go
@@ -1,1 +1,20 @@
+package cassandra
+
+import (
+	"fmt"
+	"github.com/gocql/gocql"
+	"iAccounts/cfg"
+)
+
+func GetClusterHandle(keyspace string) *(gocql.ClusterConfig) {
+	fmt.Println("KeySpace: " + keyspace)
+	cluster := gocql.NewCluster(cfg.GetCassandraClusters())
+	cluster.Keyspace = keyspace
+	return (cluster)
+}
+
+func GetSessionHandle(cluster_handle *gocql.ClusterConfig) *gocql.Session {
+	session, _ := cluster_handle.CreateSession()
+	return (session)
+}
 
